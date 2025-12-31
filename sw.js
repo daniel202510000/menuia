7d39dfada4f3c738807ff43640fc9a75ae087186
so sushi <alejandra.jandra04@gmail.com>
2025-12-31 01:56:00 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,43 @@
+const CACHE_NAME = 'menuia-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './menu.html',
+    './admin.html',
+    './manifest.json',
+    './manifest-admin.json',
+    './icon-client-gen.png',
+    './icon-admin-custom.png'
+];
+
 self.addEventListener('install', (event) => {
     console.log('[Service Worker] Install');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     console.log('[Service Worker] Activate');
     event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cache hit or network
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
 
 self.addEventListener('notificationclick', (event) => {
@@ -23,3 +55,4 @@ self.addEventListener('notificationclick', (event) => {
         })
     );
 });
+
